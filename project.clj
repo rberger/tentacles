d9745c91ebad1dcd9a70f6ad74c857817b7b89ca
dmaiocchi <dmaiocchi@suse.com>
2019-06-19 16:58:42 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject tentacles "0.5.2-SNAPSHOT"
+(defproject tentacles "0.5.3-SNAPSHOT"
   :description "A library for working with the Github API."
-  :url "https://github.com/Raynes/tentacles"
+  :url "https://github.com/clojure-garden/tentacles"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "3.1.0"]
-                 [cheshire "5.6.3"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [clj-http "3.10.0"]
+                 [cheshire "5.8.1"]
+                 ;; url has been archieved
                  [com.cemerick/url "0.1.1"]
-                 [org.clojure/data.codec "0.1.0"]
+                 [org.clojure/data.codec "0.1.1"]
                  [environ "1.1.0"]])
