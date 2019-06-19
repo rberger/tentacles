56d97c859a7446b58cecbfd7c40a352c797f3767
dmaiocchi <dmaiocchi@suse.com>
2019-06-19 17:28:20 +0200

    Add last clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/clojure-garden/tentacles"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [clj-http "3.10.0"]
                  [cheshire "5.8.1"]
                  ;; url has been archieved
